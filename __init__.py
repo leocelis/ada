48c642536292a23a2d85d5a7cedfe5f7a0fe7cf2
Leo Celis <leo@leocelis.com>
2019-01-17 13:44:51 -0300

    WIP

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 Required fiels from Facebook Ads Reporting:
 
-Country analysis: Results, Cost per Result, Cost per 1,000 People Reached
+Country analysis: Country, Results, Amount Spent (USD), Cost per Result, Cost per 1,000 People Reached
 
 Save country.csv
 """
