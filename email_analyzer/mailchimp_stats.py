8a1ddb5ea72a0a3dafb1ae2f37717d45d458134a
Leo Celis <leo@leocelis.com>
2019-04-29 18:30:52 -0300

    get mailchimp reports for a given list

--- email_analyzer/mailchimp_stats.py
+++ email_analyzer/mailchimp_stats.py
@@ -1,6 +1,3 @@
-"""
-mailchimp3 - https://pypi.org/project/mailchimp3/
-"""
 import os
 import ujson
 
@@ -10,10 +7,29 @@ from pygments import highlight, lexers, formatters
 import requests
 
 MAILCHIMP_API = os.environ.get('MAILCHIMP_API')
+MAILCHIMP_LIST_ID = os.environ.get('MAILCHIMP_LIST_ID')
+MAILCHIMP_CAMPAIGN_ID = os.environ.get('MAILCHIMP_CAMPAIGN_ID')
 headers = requests.utils.default_headers()
 client = MailChimp(mc_api=MAILCHIMP_API, timeout=30.0, request_headers=headers)
-list_id = "c0a990c819"  # the art of the ad tech
-campaign_id = "fbd62dbc6e"  # the art of the ad tech
+
+# get all reports for a given list
+r = client.reports.all(get_all=True)  # True will pull all the reports
+reports = r.get('reports', list())
+
+for item in reports:
+    if item.get('list_id') == MAILCHIMP_LIST_ID:
+        del item['_links']
+        item_json = j = ujson.dumps(item, sort_keys=True, indent=4)
+        item_json_pretty = highlight(item_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+        # print(item_json_pretty) # remove to print everything
+
+        print('"{}" | {} subs | {} opens | {}% open rate'.format(
+            item.get('subject_line'),
+            item.get('emails_sent', 0),
+            item.get('opens', {}).get('unique_opens'),
+            round(item.get('opens', {}).get('open_rate', 0) * 100, 2)
+        ))
+
 
 # # get campaigns
 # r = client.campaigns.all(get_all=False)
@@ -35,23 +51,6 @@ campaign_id = "fbd62dbc6e"  # the art of the ad tech
 
 # get reports for a campaign
 # r = client.reports.get(campaign_id=campaign_id, get_all=True)
-# o = ujson.dumps(r, sort_keys=True, indent=4)
-# j = highlight(o, lexers.JsonLexer(), formatters.TerminalFormatter())
-# print(j)
-
-# get all reports
-r = client.reports.all(get_all=False)
-reports = r.get('reports', list())
-# j = ujson.dumps(r, sort_keys=True, indent=4)
-for item in reports:
-    # only interested list
-    if item.get('list_id') == list_id:
-        del item['_links']
-        item_json = j = ujson.dumps(item, sort_keys=True, indent=4)
-        item_json_pretty = highlight(item_json, lexers.JsonLexer(), formatters.TerminalFormatter())
-        print(item_json_pretty)
-
-# r = client.reports.all(get_all=True)
 # o = ujson.dumps(r, sort_keys=True, indent=4)
 # j = highlight(o, lexers.JsonLexer(), formatters.TerminalFormatter())
 # print(j)
