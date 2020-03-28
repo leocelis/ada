47682988e083b4b57fdc5ea09e8713a988c237d1
Leo Celis <leo@leocelis.com>
2020-03-28 12:52:09 -0300

    by default pull tweets about ad tech links

--- content_analyzer/tw_most_retweeted.py
+++ content_analyzer/tw_most_retweeted.py
@@ -4,17 +4,17 @@ import sys
 
 # add parent dir
 sys.path.append(os.path.dirname(os.getcwd()))
-from ada.content_analyzer.utils import get_all_site_links, check_retweet_exists, save_link_retweets, \
+from ada.content_analyzer.utils import check_retweet_exists, save_link_retweets, \
     update_link_retweets
 from ada.content_discovery.twitter import search_tweets
 
-# from ada.utils.scrapy_sites_links import get_site_links_by_category
+from ada.utils.scrapy_sites_links import get_site_links_by_category
 
 links = dict()
 # get all site links
-site_links = get_all_site_links(domain="chiefmartec.com")
+# site_links = get_all_site_links(domain="chiefmartec.com")
 # site_links = get_all_site_links()
-# site_links = get_site_links_by_category(category='adtech')
+site_links = get_site_links_by_category(category='adtech')
 
 for s in site_links:
     link = s["site_link"]
