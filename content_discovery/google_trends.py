a8133c161e98df527bf0de25c7bdffdf61e4fdfb
Leo Celis <leo@leocelis.com>
2020-07-02 12:18:24 -0300

    g trends, related queries, related topics, related suggestions

--- content_discovery/google_trends.py
+++ content_discovery/google_trends.py
@@ -4,21 +4,35 @@ Google Trends categories: https://github.com/pat310/google-trends-api/wiki/Googl
 Run:
 > pip3 install pytrends
 Update the values in parameters
-> python3 content_discovery/google_trends.py
+$ python3 content_discovery/google_trends.py
 """
 from pytrends.request import TrendReq
 
 # parameters
-category = 12
+category = 12  # business
 geo = 'US'
-timeframe = 'today 1-m'
+timeframe = 'today 1-m'  # 30 days ago, try "today 5-y"
 gprop = ''  # default to web searches
-kw_list = ["Marketing", "Advertising"]  # keywords
+keyword = "Advertising"
+kw_list = ["Advertising"]  # keywords
 
 # build payload
 pytrends = TrendReq(hl='en-US', tz=360)
 pytrends.build_payload(kw_list, cat=category, timeframe=timeframe, geo=geo, gprop=gprop)
 
 # get related queries
+print("\n\n=================================")
+print("Related queries:")
+print("=================================")
 r = pytrends.related_queries()
+print(r)
+print("\n\n=================================")
+print("Related topics:")
+print("=================================")
+r = pytrends.related_topics()
+print(r)
+print("\n\n=================================")
+print("Suggestions:")
+print("=================================")
+r = pytrends.suggestions(keyword)
 print(r)
