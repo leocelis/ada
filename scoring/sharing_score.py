b630c3d145630fcbcfd2171c1a4358e3814cf129
Leo Celis <leo@leocelis.com>
2020-07-02 23:23:33 -0300

    remove common words

--- scoring/sharing_score.py
+++ scoring/sharing_score.py
@@ -4,36 +4,45 @@ import sys
 sys.path.append(os.path.dirname(os.getcwd()))
 from ada.predictions.utils import clean_text, get_shares_by_word, get_max_shares
 
-# input title
-test_title = "Salesforce and Ad Agencies"
 
-# normalize words
-test_title_cleaned = clean_text(test_title)
+def get_sharing_score(title=None):
+    # normalize words
+    title_cleaned = clean_text(title)
 
-# for each word, get shares
-s = 0
-for w in test_title_cleaned:
-    c = get_shares_by_word(w)
-    # if 0, remove it
-    if c == 0:
-        test_title_cleaned.remove(w)
-        continue
-    s += c
-    print("{} {}".format(w, c))
+    # for each word, get shares
+    s = 0
+    for w in title_cleaned:
+        c = get_shares_by_word(w)
+        # if 0, remove it
+        if c == 0:
+            title_cleaned.remove(w)
+            continue
+        s += c
+        print("{} {}".format(w, c))
 
-# calculate score
-print("---------------")
-max_shares = get_max_shares()  # post title max shares
-print("Max shares: {}".format(max_shares))
+    # calculate score
+    print("---------------")
+    max_shares = get_max_shares()  # post title max shares
+    print("Max shares: {}".format(max_shares))
 
-title_shares = s / len(test_title_cleaned)
-print("Title shares: {}".format(title_shares))
+    title_shares = s / len(title_cleaned)
+    print("Title shares: {}".format(title_shares))
 
-score = (title_shares * 100) / float(max_shares)
-if score >= 100:
-    score = 100
-print("Score: {}".format(score))
+    score = (title_shares * 100) / float(max_shares)
+    if score >= 100:
+        score = 100
+    print("Score: {}".format(score))
 
-# score_formatted = Decimal(score).quantize(0, ROUND_HALF_UP)
-score_formatted = round(score, 2)
-print("Score (rounded): {}".format(score_formatted))
+    # score_formatted = Decimal(score).quantize(0, ROUND_HALF_UP)
+    score_formatted = round(score, 2)
+    print("Score (rounded): {}".format(score_formatted))
+
+    return score_formatted
+
+
+def main():
+    get_sharing_score("This is a test title")
+
+
+if __name__ == "__main__":
+    main()
