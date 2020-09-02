064ae935c2b95a00ff5a62a2dc239b980a90f35e
Leo Celis <leo@leocelis.com>
2020-09-02 14:36:15 -0300

    spelling check and common words

--- predictions/utils.py
+++ predictions/utils.py
@@ -51,8 +51,8 @@ def clean_text(text):
     print("Dirty: '{}'".format(text))
     text = lower_case(text)
     text = remove_punctuation(text)
-    # text = spelling_check(text)
-    # text = remove_common_words(text)
+    text = spelling_check(text)
+    text = remove_common_words(text)
     text = lemmatize_text(text)
     print("Cleaned: {}".format(text))
 
